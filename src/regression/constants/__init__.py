7202b7ecf0f2e2b3ad9d3db1ef92eb3c29ca92ab
Kamal Singh <kamalxs321@gmail.com>
2024-09-13 15:56:21 +0530

    made few chages in constant.py file.

--- src/regression/constants/__init__.py
+++ src/regression/constants/__init__.py
@@ -1,6 +1,7 @@
 # In this module we are initialize the Constants 
 
-# from pathlib import Path
+from pathlib import Path
 
 # CONFIG_FILE_PATH = Path("config/config.yaml")
 # PARAMS_FILE_PATH = Path("params.yaml")
+# SECRET_FILE_PATH = Path("config/secrets.yaml")
